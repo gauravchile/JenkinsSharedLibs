806e42146a7de24b076705163cf234250414e537
Gaurav Chile <gauravchile05@gmail.com>
2025-11-23 20:00:13 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,10 +1,16 @@
-def call(String imageName, String tag = 'latest', String registry = '') {
+def call(String imageName, String tag = 'latest', String registry = '', String credentialsId = 'dockerhub-creds') {
     stage("Docker Push: ${imageName}:${tag}") {
         def fullImage = registry ? "${registry}/${imageName}:${tag}" : "${imageName}:${tag}"
-        echo "📤 Pushing Docker image → ${fullImage}"
-        sh """
-            docker push ${fullImage}
-        """
+        echo "📤 Preparing to push Docker image → ${fullImage}"
+
+        withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+            sh '''
+              echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+            '''
+            sh "docker push ${fullImage}"
+            sh "docker logout"
+        }
+
         echo "✅ Successfully pushed ${fullImage}"
     }
 }
