02d000d77a778da0011461a8ffa01f5613191e7f
Gaurav Chile <gauravchile05@gmail.com>
2025-11-24 01:08:54 +0530

    Update notify_email.groovy

--- vars/notify_email.groovy
+++ vars/notify_email.groovy
@@ -1,5 +1,16 @@
 def call(String to, String subject, String body) {
     stage('Notify Email') {
-        emailext(to: to, subject: subject, body: body)
+        emailext(
+            to: to,
+            subject: subject,
+            body: """
+                <h3>${subject}</h3>
+                <p>${body}</p>
+                <p>Project: ${JOB_NAME}</p>
+                <p>Build: <a href="${BUILD_URL}">#${BUILD_NUMBER}</a></p>
+            """,
+            mimeType: 'text/html',
+            attachLog: true
+        )
     }
 }
