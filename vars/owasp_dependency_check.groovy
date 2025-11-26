49a256a551e8118ee971a450b41270df6b7e8345
Gaurav Chile <gauravchile05@gmail.com>
2025-11-26 17:29:43 +0530

    Update owasp_dependency_check.groovy

--- vars/owasp_dependency_check.groovy
+++ vars/owasp_dependency_check.groovy
@@ -1,22 +1,24 @@
 def call(Map config = [:]) {
-    // Default values if not passed from the pipeline
     def scanPath   = config.get('scanPath', '.')
     def reportDir  = config.get('reportDir', "${env.WORKSPACE}/reports")
     def project    = config.get('project', 'DependencyCheck-Project')
 
     stage("📦 SCA - OWASP Dependency Check (${project})") {
         echo "🔍 Running OWASP Dependency-Check..."
+
         sh """
             mkdir -p ${reportDir}
+
             docker run --rm \
                 -v "${WORKSPACE}:/src" \
-                -v "${reportDir}:/report" \
                 owasp/dependency-check:latest \
                 --scan /src/${scanPath} \
-                --format JSON \
-                --out /report \
-                --project ${project} || true
+                --format JSON --format HTML \
+                --out /src/reports \
+                --project "${project}" || true
         """
+
         echo "✅ OWASP Dependency Check complete for ${project}. Reports stored in ${reportDir}"
+        sh "ls -lh ${reportDir} || true"
     }
 }
